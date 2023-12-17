475da915082db160ce874cd71b72c7572e73ef07
Philip Bright <philipbright@Philips-MacBook-Pro.local>
2023-12-17 19:38:22 +0630

    style: modifying landing page the UI

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,5 +1,7 @@
 
-import { pgTable, uuid,  timestamp, text } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { pgTable, uuid,  timestamp, text, jsonb, integer, boolean } from "drizzle-orm/pg-core";
+import { prices, subscriptionStatus } from "../../../migrations/schema";
 
 export const workspaces = pgTable('workspaces', {
     id: uuid('id').defaultRandom().primaryKey().notNull(),
@@ -63,4 +65,20 @@ export const workspaces = pgTable('workspaces', {
       }),
   });
 
-  
+  export const subscriptions = pgTable("subscriptions", {
+    id: text("id").primaryKey().notNull(),
+    userId: uuid("user_id").notNull(),
+    status: subscriptionStatus("status"),
+    metadata: jsonb("metadata"),
+    priceId: text("price_id").references(() => prices.id),
+    quantity: integer("quantity"),
+    cancelAtPeriodEnd: boolean("cancel_at_period_end"),
+    created: timestamp("created", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
+    currentPeriodStart: timestamp("current_period_start", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
+    currentPeriodEnd: timestamp("current_period_end", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
+    endedAt: timestamp("ended_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+    cancelAt: timestamp("cancel_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+    canceledAt: timestamp("canceled_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+    trialStart: timestamp("trial_start", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+    trialEnd: timestamp("trial_end", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+  });
