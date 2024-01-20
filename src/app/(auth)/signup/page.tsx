fc2296f59ab4b3991944236afab03cd0891945ba
PhilipBright <philipphoo123@gmail.com>
2024-01-20 23:56:12 +0630

    style: modifying signup form

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,13 +1,21 @@
 'use client';
-import { Form } from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
 import clsx from 'clsx'
+import Image from 'next/image';
+import Link from 'next/link';
 import { useSearchParams } from 'next/navigation'
 import { useRouter } from 'next/router'
 import React, { useMemo, useState } from 'react'
 import { useForm } from 'react-hook-form';
-
+import Logo from '../../../../public/cypresslogo.svg'
 import {z} from 'zod'
+import Loader from '@/components/global/Loader';
+import { MailCheck } from 'lucide-react';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { FormSchema } from '@/lib/types';
 
 const SignUpFormSchema = z.object({
     email: z.string().describe('Email').email({message: 'Invalid Email'}),
@@ -23,7 +31,7 @@ const Signup = () => {
     const searchParams = useSearchParams();
     const [submitError, setSubmitError] = useState('');
     const [confirmation, setConfirmation] = useState(false)
-    const constExchangeError = useMemo(() => {
+    const codeExchangeError = useMemo(() => {
         if (!searchParams) {
           return '';
         }
@@ -31,9 +39,9 @@ const Signup = () => {
     }, [searchParams]);
 
     const confirmationAndErrorStyles = useMemo(()=> clsx('bg-primary', {
-        "bg-red-500/10":constExchangeError,
-        'border-red-500/50': constExchangeError,
-        'text-red-700': constExchangeError
+        "bg-red-500/10":codeExchangeError,
+        'border-red-500/50': codeExchangeError,
+        'text-red-700': codeExchangeError
     }), [])
 
     const form = useForm<z.infer<typeof SignUpFormSchema>>({
@@ -41,8 +49,8 @@ const Signup = () => {
         resolver: zodResolver(SignUpFormSchema),
         defaultValues: {email: '', password: '', confirmPassword: ''},
     })
-
-    const onSubmit = () => {
+    const isLoading = form.formState.isSubmitting
+    const onSubmit = async ({ email, password}: z.infer<typeof FormSchema>) => {
     }
     const signUpHandler = () => {}
   return (
@@ -53,7 +61,119 @@ const Signup = () => {
     }}
     onSubmit={form.handleSubmit(onSubmit)}
     className='w-full sm:justify-center sm-w-[400px] space-y-6 flex flex-col'
-    ></form>
+    >
+        <Link
+          href="/"
+          className="
+          w-full
+          flex
+          justify-left
+          items-center"
+        >
+          <Image
+            src={Logo}
+            alt="cypress Logo"
+            width={50}
+            height={50}
+          />
+          <span
+            className="font-semibold
+          dark:text-white text-4xl first-letter:ml-2"
+          >
+            cypress.
+          </span>
+        </Link>
+        <FormDescription
+          className="
+        text-foreground/60"
+        >
+          An all-In-One Collaboration and Productivity Platform
+        </FormDescription>
+        {!confirmation && !codeExchangeError && (
+        <>
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Confirm Password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+            <Button
+            type='submit'
+            className='w-full p-6'
+            disabled={isLoading}
+            >
+                {!isLoading ? 'Create Account' : <Loader/>}
+            </Button>
+        </>
+        )}
+       
+        {submitError && <FormMessage>{submitError}</FormMessage>}
+        
+        <span className="self-container">
+          Already have an account?{' '}
+          <Link
+            href="/login"
+            className="text-primary"
+          >
+           Login
+          </Link>
+          </span>
+          {(confirmation || codeExchangeError) && <>
+            <Alert className={confirmationAndErrorStyles}>
+                {!codeExchangeError  && <MailCheck className='h-4 w-4' />}
+                {' '}
+                <AlertTitle>{codeExchangeError ? 'Invalid Link' : 'Check your email'}</AlertTitle>
+  <AlertDescription>
+    {codeExchangeError || 'An email confirmation has been sent'}
+  </AlertDescription>
+            </Alert>
+          </>}
+    </form>
     </Form>
   )
 }
--- src/lib/server-action/server-actions.ts
+++ src/lib/server-action/server-actions.ts
@@ -9,7 +9,23 @@ export async function actionLoginUser({email, password}: z.infer<typeof FormSche
     const supabase = createRouteHandlerClient({ cookies })
     const response  = await supabase.auth.signInWithPassword({
         email,
-        password
+        password 
+    })
+    return response
+}
+export async function actionSignUpUser({
+    email, password
+}: z.infer<typeof FormSchema>){
+    const supabase = createRouteHandlerClient({cookies});
+    const {data} = await supabase.from('profiles').select('*').eq('email', email)
+
+    if(data?.length) return {error: {message: 'User Already Exists', data}}
+    const response = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}api/auth/callback`,
+        }
     })
     return response
 }
